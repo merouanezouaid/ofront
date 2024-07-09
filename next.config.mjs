e0a7d8d15e7211a4828fa6576f179b01b7bc66e3
merouanezouaid <marwan.zouaid@gmail.com>
2024-07-09 19:55:02 +0100

    fixed stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async headers() {
+      return [
+        {
+          source: '/socket.io/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+          ],
+        },
+      ]
+    },
     async rewrites() {
       return [
         {
           source: '/socket.io/:path*',
           destination: 'http://64.227.124.78/socket.io/:path*',
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
         },
       ];
     },
