70879cb4252365071e24307f7c2d755e26e63231
merouanezouaid <marwan.zouaid@gmail.com>
2024-07-24 03:05:13 +0100

    changed video

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -6,8 +6,8 @@ let remoteSocket: string = '';
 let type: string = '';
 let roomid: string = '';
 
-// const myVideoElement: HTMLVideoElement | null = null;
-// const strangerVideoElement: HTMLVideoElement | null = null;
+const myVideoElement: HTMLVideoElement | null = null;
+const strangerVideoElement: HTMLVideoElement | null = null;
 
 export function initializeSocket(onQuestion: (q: string) => void, onLoading: (loading: boolean) => void, onMessage: (sender: string, text: string) => void, myVideoRef: HTMLVideoElement,
 strangerVideoRef: HTMLVideoElement) {
@@ -36,7 +36,7 @@ strangerVideoRef: HTMLVideoElement) {
         socket?.emit('ice:send', { candidate: e.candidate, to: remoteSocket });
       };
 
-      // initializeWebRTC(myVideoRef, strangerVideoRef)
+      initializeWebRTC(myVideoRef, strangerVideoRef)
 
     });
 
@@ -59,43 +59,43 @@ strangerVideoRef: HTMLVideoElement) {
 }
 
 
-// export function initializeWebRTC(myVideoRef: HTMLVideoElement, strangerVideoRef: HTMLVideoElement) {
-//   navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-//     .then(stream => {
-//       if(peer){
-//         myVideoRef!.srcObject = stream;
-//         stream.getTracks().forEach(track => peer!.addTrack(track, stream));
+export function initializeWebRTC(myVideoRef: HTMLVideoElement, strangerVideoRef: HTMLVideoElement) {
+  navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+    .then(stream => {
+      if(peer){
+        myVideoRef!.srcObject = stream;
+        stream.getTracks().forEach(track => peer!.addTrack(track, stream));
   
-//         console.log("peer: " + peer)
-//         console.log("on track" + peer.ontrack)
+        console.log("peer: " + peer)
+        console.log("on track" + peer.ontrack)
   
-//         peer.ontrack = e => {
-//           strangerVideoRef!.srcObject = e.streams[0];
-//           strangerVideoRef!.play().catch(error => {
-//             console.error('Error playing video:', error);
-//           });
-//         };
-//       }
-//     })
-//     .catch(ex => {
-//       console.log(ex);
-//     });
+        peer.ontrack = e => {
+          strangerVideoRef!.srcObject = e.streams[0];
+          strangerVideoRef!.play().catch(error => {
+            console.error('Error playing video:', error);
+          });
+        };
+      }
+    })
+    .catch(ex => {
+      console.log(ex);
+    });
 
-//   if (socket) {
-//     socket.on('sdp:reply', async ({ sdp }: { sdp: RTCSessionDescriptionInit }) => {
-//       await peer!.setRemoteDescription(new RTCSessionDescription(sdp));
-//       if (type === 'p2') {
-//         const ans = await peer!.createAnswer();
-//         await peer!.setLocalDescription(ans);
-//         socket?.emit('sdp:send', { sdp: peer!.localDescription });
-//       }
-//     });
+  if (socket) {
+    socket.on('sdp:reply', async ({ sdp }: { sdp: RTCSessionDescriptionInit }) => {
+      await peer!.setRemoteDescription(new RTCSessionDescription(sdp));
+      if (type === 'p2') {
+        const ans = await peer!.createAnswer();
+        await peer!.setLocalDescription(ans);
+        socket?.emit('sdp:send', { sdp: peer!.localDescription });
+      }
+    });
 
-//     socket.on('ice:reply', async ({ candidate }: { candidate: RTCIceCandidateInit }) => {
-//       await peer!.addIceCandidate(candidate);
-//     });
-//   }
-// }
+    socket.on('ice:reply', async ({ candidate }: { candidate: RTCIceCandidateInit }) => {
+      await peer!.addIceCandidate(candidate);
+    });
+  }
+}
 
 export function sendMessage(message: string) {
   socket?.emit('send-message', message, type, roomid);
